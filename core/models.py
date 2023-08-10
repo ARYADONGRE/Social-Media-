c443c2263da3a57fa09c01df10ed3604b8f022e4
Arya Adwait Dongre <87186206+ARYADONGRE@users.noreply.github.com>
2023-08-10 11:56:45 +0530

    Commit 1
    core , media , social media

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 User=get_user_model()
 class Profile(models.Model):
@@ -10,3 +13,25 @@ class Profile(models.Model):
     location =models.CharField(max_length=100,blank=True)
     def __str__ (self):
         return self.user.username
+    
+class Post(models.Model):
+    id= models.UUIDField(primary_key=True,default=uuid.uuid4)
+    user= models.CharField(max_length=100)
+    image=models.ImageField(upload_to='post_images')
+    caption=models.TextField()
+    created_at=models.DateTimeField(default=datetime.now)
+    no_of_likes=models.IntegerField(default=0)
+    def __str__(self):
+        return self.user
+    
+class LikePost(models.Model):
+    post_id=models.CharField(max_length=100)
+    username=models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
+    
+class FollowersCount(models.Model):
+    follower=models.CharField(max_length=100)
+    user=models.CharField(max_length=100)
+    def __str__(self):
+        return self.user
--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,10 @@ urlpatterns=[
     path('signup',views.signup,name='signup'),
     path('signin',views.signin,name='signin'),
     path('logout',views.logout,name='logout'),
-    path('settings',views.settings,name='settings')
+    path('settings',views.settings,name='settings'),
+    path('upload',views.upload,name='upload'),
+    path('like-post',views.like_post,name='like-post'),
+    path('profile/<str:pk>',views.profile,name='profile'),
+    path('follow',views.follow,name='follow'),
+    path('search',views.search,name='search')
 ]
